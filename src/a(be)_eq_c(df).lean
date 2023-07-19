507019b10283ff5ea0ad7f24c9bb0102cb3a24c8
Jose A. Alonso <JoseA.Alonso@gmail.com>
2023-07-19 13:40:42 +0200

    Si ab = cd y e = f, entonces a(be) = c(df).

--- src/a(be)_eq_c(df).lean
+++ src/a(be)_eq_c(df).lean
@@ -34,11 +34,12 @@ example
   (h1 : a * b = c * d)
   (h2 : e = f)
   : a * (b * e) = c * (d * f) :=
-by
-  rw [h2]
-  rw [←mul_assoc]
-  rw [h1]
-  rw [mul_assoc]
+calc
+  a * (b * e)
+    = a * (b * f) := by rw [h2]
+  _ = (a * b) * f := by rw [←mul_assoc]
+  _ = (c * d) * f := by rw [h1]
+  _ = c * (d * f) := by rw [mul_assoc]
 
 -- 2ª demostración
 example
@@ -46,12 +47,11 @@ example
   (h1 : a * b = c * d)
   (h2 : e = f)
   : a * (b * e) = c * (d * f) :=
-calc
-  a * (b * e)
-    = a * (b * f) := by rw [h2]
-  _ = (a * b) * f := by rw [←mul_assoc]
-  _ = (c * d) * f := by rw [h1]
-  _ = c * (d * f) := by rw [mul_assoc]
+by
+  rw [h2]
+  rw [←mul_assoc]
+  rw [h1]
+  rw [mul_assoc]
 
 -- 3ª demostración
 example
