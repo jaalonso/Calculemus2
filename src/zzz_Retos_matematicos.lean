376730c795c1b2a13b8baba303ecd12c2a8b38f7
Jose A. Alonso <JoseA.Alonso@gmail.com>
2025-07-17 17:36:04 +0200

    Añade ejemplo para "Retos matemáticos"

--- src/zzz_Retos_matematicos.lean
+++ src/zzz_Retos_matematicos.lean
@@ -5,3 +5,6 @@ by tauto
 
 example : 2 + 2 = 5 → (∀ n, n > 2 → ¬∃ x y z, x > 0 ∧ y > 0 ∧ z > 0 ∧ x^n + y^n ≠ z^n) :=
 by tauto
+
+example : 2 + 2 = 5 → (∀ n,  n > 2 → Even n → ∃ p q : ℕ, Prime p ∧ Prime q ∧ n = p + q) :=
+by tauto
