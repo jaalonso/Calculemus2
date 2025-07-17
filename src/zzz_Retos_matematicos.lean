c548352a0e4264f79d4f39b8a06e1a75b1e986cf
Jose A. Alonso <JoseA.Alonso@gmail.com>
2025-07-17 17:24:26 +0200

    Añade ejemplo para "Retos matemáticos"

--- src/zzz_Retos_matematicos.lean
+++ src/zzz_Retos_matematicos.lean
@@ -3,5 +3,5 @@ import Mathlib.Tactic
 example : 2 + 2 = 5 → 0 = 1 :=
 by tauto
 
-example : 2 + 2 = 5 → (∀ x y z n : ℕ, n > 2 → x * y * z ≠ 0 → x^n + y^n ≠ z^n) :=
+example : 2 + 2 = 5 → (∀ n, n > 2 → ¬∃ x y z, x > 0 ∧ y > 0 ∧ z > 0 ∧ x^n + y^n ≠ z^n) :=
 by tauto
